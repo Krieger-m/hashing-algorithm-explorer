432665d7de24aaf12305ac172f5dd3b6ef745e8c
Mat <matze.krieger@googlemail.com>
2026-05-06 23:29:21 +0200

    added textFields

---
+++ src/app/_components/InputField.tsx
@@ -0,0 +1,34 @@
+"use client";
+import styles from "./styles/components.module.css";
+import { useContext } from "react";
+import { HashContext } from "../CryptoContext";
+
+interface InputFieldProps {
+  label: string;
+}
+
+export default function InputField(props: InputFieldProps) {
+  const { label } = props;
+  const context = useContext(HashContext);
+
+  if (!context) return null;
+  const { input, setInput } = context;
+
+  return (
+    <div>
+      <p>{label}</p>
+      <input
+        type="text"
+        placeholder="your input"
+        className={styles.textField}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            setInput(e.currentTarget.value);
+          }
+        }}
+      />
+    </div>
+  );
+}
---
+++ src/app/_components/OutputField.tsx
@@ -0,0 +1,29 @@
+'use client';
+import styles from "./styles/components.module.css";
+import { useContext } from "react";
+import { HashContext } from "../CryptoContext";
+
+interface OutputFieldProps {
+  label: string;
+}
+
+export default function OutputField(props: OutputFieldProps) {
+  const { label } = props;
+  const context = useContext(HashContext);
+
+  if (!context) return null;
+  const { output } = context;
+
+  return (
+    <>
+      <p>{label}</p>
+      <input 
+        type="text" 
+        className={styles.textField} 
+        placeholder="your output" 
+        value={output} 
+        readOnly 
+      />
+    </>
+  );
+}
