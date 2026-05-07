396f2cc6d1bb73dbe7b28bf08d28aea07e3f263d
Mat <matze.krieger@googlemail.com>
2026-05-07 20:31:35 +0200

    added seedField to salt complex hash functions

---
+++ src/app/_components/SeedField.tsx
@@ -0,0 +1,37 @@
+"use client";
+import styles from "./styles/components.module.css";
+import { useContext } from "react";
+import { HashContext } from "../HashContext";
+import { Spacing } from "./Spacing";
+
+interface SeedFieldProps {
+  label: string;
+}
+
+export default function SeedField(props: SeedFieldProps) {
+  const { label } = props;
+  const context = useContext(HashContext);
+
+  if (!context) return null;
+  const { seed, setSeed } = context;
+
+  if (context.algorithm == "hmac")
+    return (
+      <div>
+        <p>{label}</p>
+        <input
+          type="text"
+          placeholder="seed input"
+          className={styles.textField}
+          value={seed}
+          onChange={(e) => setSeed(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              setSeed(e.currentTarget.value);
+            }
+          }}
+        />
+        <Spacing height={2} />
+      </div>
+    );
+}
