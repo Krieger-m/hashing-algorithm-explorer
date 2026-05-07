3b4e8ebce7636fc797b992158de80a62563446a3
Mat <matze.krieger@googlemail.com>
2026-05-07 20:33:03 +0200

    modified selector

--- src/app/_components/Selector.tsx
+++ src/app/_components/Selector.tsx
@@ -1,28 +1,36 @@
-'use client';
-import styles from './styles/components.module.css';
-import { useContext } from 'react';
-import { HashContext } from '../CryptoContext';
+"use client";
+import styles from "./styles/components.module.css";
+import { useContext } from "react";
+import { HashContext } from "../HashContext";
 
-export default function Selector(props:{label:string}){
-    const {label} = props;
-    const context = useContext(HashContext);
+export default function Selector(props: { label: string }) {
+  const { label } = props;
+  const context = useContext(HashContext);
 
-    if (!context) return null;
-    const { algorithm, setAlgorithm } = context;
+  if (!context) return null;
+  const { algorithm, setAlgorithm } = context;
 
-    return(
-        <>
-            <label>{label}</label>
-            <select 
-                className={styles.selector} 
-                value={algorithm} 
-                onChange={(e) => setAlgorithm(e.target.value)}
-            >
-                <option className={styles.optionStyle} value='sha256'>sha256</option>
-                <option className={styles.optionStyle} value='hmac'>hmac</option>
-                <option className={styles.optionStyle} value='md5'>md5</option>
-                <option className={styles.optionStyle} value='sha1'>sha1</option>
-            </select>
-        </>
-    )
+  return (
+    <>
+      <label>{label}</label>
+      <select
+        className={styles.selector}
+        value={algorithm}
+        onChange={(e) => setAlgorithm(e.target.value)}
+      >
+        <option className={styles.optionStyle} value="sha256">
+          sha256
+        </option>
+        <option className={styles.optionStyle} value="hmac">
+          hmac
+        </option>
+        <option className={styles.optionStyle} value="md5">
+          md5
+        </option>
+        <option className={styles.optionStyle} value="sha1">
+          sha1
+        </option>
+      </select>
+    </>
+  );
 }
