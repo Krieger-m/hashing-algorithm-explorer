37ffae3217520c26cff1d34c212b2dd918c3023b
Mat <matze.krieger@googlemail.com>
2026-05-07 20:32:09 +0200

    renamed hash-functions and modified HMAC hash to use dynamic seed

---
+++ src/app/_utils/hash-functions.ts
@@ -0,0 +1,27 @@
+import crypto from "crypto";
+
+/**
+ * Generates a SHA-256 hash of the input string.
+ * @param input The string to hash.
+ * @returns The hex-encoded hash string.
+ */
+
+export function hashSha256(inp: string) {
+  const x = crypto.createHash("sha256").update(inp).digest("hex");
+  console.log(x);
+  return x;
+}
+
+export function hashHMAC(inp: string, seed: string ) {
+  const x = crypto.createHmac("sha512", seed).update(inp).digest("hex");
+  console.log(x);
+  return x;
+}
+
+export function hashMd5(inp: string) {
+  return crypto.createHash("md5").update(inp).digest("hex");
+}
+
+export function hashSha1(inp: string) {
+  return crypto.createHash("sha1").update(inp).digest("hex");
+}
