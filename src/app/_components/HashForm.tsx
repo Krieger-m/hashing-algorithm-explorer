8fc1116fa8e58fadfaf0bf94d7db17c268927ecc
Mat <matze.krieger@googlemail.com>
2026-05-07 20:30:56 +0200

    renamed HashForm and added SeedField

--- src/app/_components/HashForm.tsx
+++ src/app/_components/HashForm.tsx
@@ -1,21 +1,25 @@
-import { HashContextProvider } from "../CryptoContext";
+"use client";
+import { HashContextProvider } from "../HashContext";
 import InputField from "./InputField";
 import OutputField from "./OutputField";
+import SeedField from "./SeedField";
 import Selector from "./Selector";
 import { Spacing } from "./Spacing";
 
-export default function CryptoForm() {
+export default function HashForm() {
   return (
     <>
       <HashContextProvider>
         <InputField label="Input" />
-  
+
         <Spacing height={2} />
-  
+
+        <SeedField label="Seed" />
+
         <Selector label="Select algorithm" />
-  
+
         <Spacing height={2} />
-  
+
         <OutputField label="Output" />
       </HashContextProvider>
     </>
