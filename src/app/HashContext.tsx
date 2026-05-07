d5877c2ff89ea0c67d514bd129a4619ed7d9039d
Mat <matze.krieger@googlemail.com>
2026-05-07 20:30:33 +0200

    renamed hashContext

--- src/app/HashContext.tsx
+++ src/app/HashContext.tsx
@@ -1,16 +1,25 @@
-'use client';
+"use client";
 import { createContext, useState, ReactNode, useMemo } from "react";
-import { hashSha256, hashMd5, hashSha1, hashHMAC } from "./_utils/hash";
+import {
+  hashSha256,
+  hashMd5,
+  hashSha1,
+  hashHMAC,
+} from "./_utils/hash-functions";
 
 interface HashContextType {
   input: string;
   setInput: (val: string) => void;
+  seed: string;
+  setSeed: (val: string) => void;
   algorithm: string;
   setAlgorithm: (val: string) => void;
   output: string;
 }
 
-export const HashContext = createContext<HashContextType | undefined>(undefined);
+export const HashContext = createContext<HashContextType | undefined>(
+  undefined,
+);
 
 interface HashContextProviderProps {
   children?: ReactNode;
@@ -18,28 +27,40 @@ interface HashContextProviderProps {
 
 export function HashContextProvider(props: HashContextProviderProps) {
   const { children } = props;
+
   const [input, setInput] = useState("");
+  const [seed, setSeed] = useState("");
   const [algorithm, setAlgorithm] = useState("sha256");
 
   const output = useMemo(() => {
     if (!input) return "";
 
     switch (algorithm) {
-      case 'sha256':
+      case "sha256":
         return hashSha256(input);
-      case 'md5':
+      case "md5":
         return hashMd5(input);
-      case 'sha1':
+      case "sha1":
         return hashSha1(input);
-      case 'hmac':
-        return hashHMAC(input);
+      case "hmac":
+        return hashHMAC(input,seed);
       default:
-        return 'Unknown algorithm';
+        return "Unknown algorithm";
     }
-  }, [input, algorithm]);
+  }, [input, algorithm, seed]);
 
   return (
-    <HashContext.Provider value={{ input, setInput, algorithm, setAlgorithm, output }}>
+    <HashContext.Provider
+      value={{
+        input,
+        setInput,
+        algorithm,
+        setAlgorithm,
+        seed,
+        setSeed,
+        output,
+      }}
+    >
       {children}
     </HashContext.Provider>
   );
