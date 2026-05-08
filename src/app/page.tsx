210b760eed7c3aa130a909f7175a64cc1c1a2d81
Mat <matze.krieger@googlemail.com>
2026-05-09 00:08:02 +0200

    optimized page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,23 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
         <h1>Explore Hash Algorithms in Real Time</h1>
         <Spacing height={2} />
-        <p>Enter text to compute its hash using the selected algorithm.<br/>
-All hashing happens client‑side. No network requests. No data leaves your browser.</p>
+        <p>
+          Enter text to compute its hash using the selected algorithm.
+          <br />
+          All hashing happens client‑side. No network requests. No data leaves
+          your browser.
+        </p>
         <Spacing height={4} />
-        <HashForm/>
-        <Spacing height={2}/>
+        <HashForm />
+        <Spacing height={6} />
+        <div className={styles.ctas}>
+          <a className={styles.secondary} href="https://mk-dev.org/projects">
+            Back
+          </a>
+        </div>
+        <Spacing height={2} />
       </main>
     </div>
   );
